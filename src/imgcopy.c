3696de6e6956e7426c23c88b4fc1b3bc4f415a44
Eduardo Cardozo <eduard.cardoz@gmail.com>
2022-08-12 03:41:47 -0300

    refactor: creates the imgsize object to handle the object width and height

--- src/imgcopy.c
+++ src/imgcopy.c
@@ -6,10 +6,19 @@
 
 #define byte unsigned char
 
+struct ImgSize
+{
+    int height;
+    int width;
+};
+
+typedef struct ImgSize ImgSize;
+
+ImgSize getImgSize(const char *image_name);
+
 void imageReader(
     const char *image_name,
-    int *_height,
-    int *_width,
+    const ImgSize *imgSize,
     int *_bitDepth,
     byte *_header,
     byte *_colorTable,
@@ -17,8 +26,7 @@ void imageReader(
 
 void imageWritter(
     const char *imageName,
-    const int *_height,
-    const int *_width,
+    const ImgSize *imgSize,
     byte *header,
     byte *colorTable,
     byte *buffer,
@@ -35,16 +43,43 @@ int main()
     const char imgName[] = "images/man.bmp";
     const char copyImgName[] = "images/man_copy.bmp";
 
-    imageReader(imgName, &height, &width, &bitDepth, header, colorTable, buffer);
-    imageWritter(copyImgName, &height, &width, header, colorTable, buffer, bitDepth);
+    ImgSize imgSize = getImgSize(imgName);
+
+    imageReader(imgName, &imgSize, &bitDepth, header, colorTable, buffer);
+    imageWritter(copyImgName, &imgSize, header, colorTable, buffer, bitDepth);
 
     printf("Success! \n");
 }
 
+ImgSize getImgSize(const char *image_name)
+{
+    byte _header[BMP_HEADER_SIZE];
+    FILE *stream_in;
+
+    stream_in = fopen("images/cameraman.bmp", "rb");
+
+    if (stream_in == (FILE *)0)
+    {
+        printf("Unable to open the file\n");
+        exit(1);
+    }
+
+    for (size_t i = 0; i < 54; i++)
+    {
+        _header[i] = getc(stream_in);
+    }
+
+    ImgSize s = {
+        height : *(int *)&_header[22],
+        width : *(int *)&_header[18]
+    };
+
+    return s;
+}
+
 void imageReader(
     const char *image_name,
-    int *_height,
-    int *_width,
+    const ImgSize *imgSize,
     int *_bitDepth,
     byte *_header,
     byte *_colorTable,
@@ -65,8 +100,6 @@ void imageReader(
         _header[i] = getc(stream_in);
     }
 
-    *_width = *(int *)&_header[18];
-    *_height = *(int *)&_header[22];
     *_bitDepth = *(int *)&_header[28];
 
     if (*_bitDepth <= 8)
@@ -74,17 +107,15 @@ void imageReader(
         fread(_colorTable, sizeof(byte), 1024, stream_in);
     }
 
-    int imgSize = *_height * *_width;
-
-    fread(buffer, sizeof(byte), imgSize, stream_in);
+    int _imgSize = imgSize->height * imgSize->width;
+    fread(buffer, sizeof(byte), _imgSize, stream_in);
 
     fclose(stream_in);
 }
 
 void imageWritter(
     const char *imageName,
-    const int *_height,
-    const int *_width,
+    const ImgSize *imgSize,
     byte *header,
     byte *colorTable,
     byte *buffer,
@@ -100,9 +131,8 @@ void imageWritter(
         fwrite(colorTable, sizeof(byte), 1024, stream_out);
     }
 
-    int imgSize = *_height * *_width;
-
-    fwrite(buffer, sizeof(byte), imgSize, stream_out);
+    int _imgSize = imgSize->height * imgSize->width;
+    fwrite(buffer, sizeof(byte), _imgSize, stream_out);
 
     fclose(stream_out);
 }
